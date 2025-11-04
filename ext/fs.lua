c1580b674e89676015393531b3a778f24a7a3f57
Cothy <cothyoff@mailhaven.su>
2025-11-04 12:46:24 +0100

    Update fs.lua

--- ext/fs.lua
+++ ext/fs.lua
@@ -1,4 +1,15 @@
+-- SimpleFS – Complete File System Wrapper for Potassium FS
 local SimpleFS = {}
+
+-- Check if a file exists
+function SimpleFS.existsFile(path)
+    return isfile(path)
+end
+
+-- Check if a folder exists
+function SimpleFS.existsFolder(path)
+    return isfolder(path)
+end
 
 -- Read the contents of a file
 function SimpleFS.read(path)
@@ -9,12 +20,15 @@ function SimpleFS.read(path)
     end
 end
 
--- Write data to a file
+-- Write data to a file (overwrites if exists)
 function SimpleFS.write(path, data)
+    if isfile(path) then
+        delfile(path)
+    end
     writefile(path, data)
 end
 
--- Append data to a file
+-- Append data to a file (creates if missing)
 function SimpleFS.append(path, data)
     appendfile(path, data)
 end
@@ -32,8 +46,6 @@ end
 function SimpleFS.createFolder(path)
     if not isfolder(path) then
         makefolder(path)
-    else
-        error("Folder already exists: " .. path)
     end
 end
 
@@ -55,6 +67,15 @@ function SimpleFS.list(path)
     end
 end
 
+-- Load a Lua file as a function
+function SimpleFS.load(path, chunkname)
+    if isfile(path) then
+        return loadfile(path, chunkname)
+    else
+        error("File does not exist: " .. path)
+    end
+end
+
 -- Execute a Lua file
 function SimpleFS.run(path)
     if isfile(path) then
@@ -62,6 +83,11 @@ function SimpleFS.run(path)
     else
         error("File does not exist: " .. path)
     end
+end
+
+-- Convenience: create or overwrite a file
+function SimpleFS.createFile(path, data)
+    writefile(path, data)
 end
 
 return SimpleFS
